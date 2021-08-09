14a8807b2911fd57c014689812dc23df75521787
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-08-09 22:12:56 +0530

    'todoapp'

--- App.js
+++ App.js
@@ -6,44 +6,37 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "vaibhav", key: 1 },
-    { name: "rishabh", key: 2 },
-    { name: "pratham", key: 3 },
-    { name: "sahil", key: 4 },
-    { name: "aananya", key: 5 },
-    { name: "radhika", key: 6 },
-    { name: "nidhi", key: 7 },
-    { name: "hritika", key: 8 },
+  const [todos, setTodos] = useState([
+    { text: "Learn React", key: "1" },
+    { text: "Learn React Native", key: "2" },
+    { text: "Learn Node js", key: "3" },
   ]);
 
-  const pressHandler = (id) => {
-    alert(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter((person) => person.key != id);
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.key != key);
     });
   };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      {/* <ScrollView>
-        {people.map((item) => (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        ))}
-      </ScrollView> */}
+      {/* header */}
+      <Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -52,16 +45,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 20,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
---
+++ Test.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+export default function Test() {
+  const [people, setPeople] = useState([
+    { name: "vaibhav", key: 1 },
+    { name: "rishabh", key: 2 },
+    { name: "pratham", key: 3 },
+    { name: "sahil", key: 4 },
+    { name: "aananya", key: 5 },
+    { name: "radhika", key: 6 },
+    { name: "nidhi", key: 7 },
+    { name: "hritika", key: 8 },
+  ]);
+
+  const pressHandler = (id) => {
+    alert(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.key != id);
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        numColumns={2}
+        data={people}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+      {/* <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: "pink",
+    fontSize: 24,
+    marginHorizontal: 20,
+  },
+});
