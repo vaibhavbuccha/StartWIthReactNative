e67de1f3fe2cf0c0b31b881eb1a7b4d667659256
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-08-11 20:01:19 +0530

    'COmpleteTODOApp'

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import AddTodo from "./components/AddTodo";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 
@@ -15,6 +16,12 @@ export default function App() {
     { text: "Learn React Native", key: "2" },
     { text: "Learn Node js", key: "3" },
   ]);
+
+  const submitHander = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
@@ -26,6 +33,7 @@ export default function App() {
     <View style={styles.container}>
       {/* header */}
       <Header />
+      <AddTodo submitHander={submitHander} />
       <View style={styles.content}>
         {/* todo form */}
         <View style={styles.list}>
---
+++ components/AddTodo.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+const AddTodo = ({ submitHander }) => {
+  const [text, setText] = useState("");
+  const changeHandler = (val) => {
+    setText(val);
+  };
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="New Todo...."
+        onChangeText={(val) => changeHandler(val)}
+      />
+      <Button
+        title="Add todo"
+        onPress={() => submitHander(text)}
+        color="coral"
+      />
+    </View>
+  );
+};
+
+export default AddTodo;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 40,
+  },
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+});
